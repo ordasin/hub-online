e1d318c2221b927c5259a7395d70f43df9f29c27
ordasin <daniordastena@gmail.com>
2026-02-03 07:14:05 +0100

    UI: Change OrdasinOptimizer image to cartoon tech style

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -10,8 +10,8 @@ export const projects = [
     createdAt: '01 Feb 2026',
     system: 'Windows',
     images: [
-      'https://images.unsplash.com/photo-1550751827-4bd374c3f58b?auto=format&fit=crop&q=80&w=1200',
-      'https://images.unsplash.com/photo-1518770660439-4636190af475?auto=format&fit=crop&q=80&w=1200'
+      'https://images.unsplash.com/photo-1614728263952-84ea256f9679?auto=format&fit=crop&q=80&w=1200',
+      'https://images.unsplash.com/photo-1542751371-adc38448a05e?auto=format&fit=crop&q=80&w=1200'
     ]
   },
   {
